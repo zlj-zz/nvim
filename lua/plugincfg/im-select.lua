a70ec3e3b4edbf00c504b22e85d1d2dc2da313cc
Haha <haha@HahadeMacBook-Pro.local>
2026-05-12 14:09:06 +0800

    refactor: make im-select with nativi lua
    - im-select.lua: async jobstart for all im-select calls on hot paths
    - im-select.lua: use utils.autocmd instead of raw nvim_create_autocm

--- lua/plugincfg/im-select.lua
+++ lua/plugincfg/im-select.lua
@@ -1,43 +1,40 @@
--- macOS implementations auto select
+-- macOS input method auto-switch
 -- deps: im-select (https://github.com/daipeihust/im-select)
 
--- only enable at macOS
-local sysname = vim.loop.os_uname().sysname
-if sysname ~= 'Darwin' then
+local uv = vim.uv or vim.loop
+if uv.os_uname().sysname ~= 'Darwin' then
     return
 end
 
 local im_select = 'im-select'
 local utils = require('utils')
 
--- check im-select  is usable
+-- check im-select is usable
 if vim.fn.executable(im_select) == 0 then
     local install_cmd = 'brew tap daipeihust/tap && brew install im-select'
 
-    vim.api.nvim_create_autocmd('VimEnter', {
-        once = true,
-        callback = function()
+    utils.autocmd('IMSelectMissing', {
+        { 'VimEnter', once = true, callback = function()
             utils.warn(
                 'im-select not found. Input method auto-switch disabled.\n'
                 .. 'Install with: ' .. install_cmd
             )
-        end,
-    })
+        end },
+    }, true)
 
-    -- 提供 :IMSelectInstall 命令一键安装
     vim.api.nvim_create_user_command('IMSelectInstall', function()
-        vim.notify('Running: ' .. install_cmd, vim.log.levels.INFO)
+        utils.info('Running: %s', install_cmd)
         local job = vim.fn.jobstart({ 'sh', '-c', install_cmd }, {
             on_exit = function(_, code)
                 if code == 0 then
                     utils.info('im-select installed. Please restart Neovim.')
                 else
-                    utils.error('im-select install failed. Run manually:\n' .. install_cmd)
+                    utils.error('im-select install failed. Run manually:\n%s', install_cmd)
                 end
             end,
             on_stderr = function(_, data)
-                if data and data[1] ~= '' then
-                    vim.notify(table.concat(data, '\n'), vim.log.levels.WARN)
+                if data and #data > 0 and data[1] ~= '' then
+                    utils.warn('%s', table.concat(data, '\n'))
                 end
             end,
         })
@@ -49,38 +46,44 @@ if vim.fn.executable(im_select) == 0 then
     return
 end
 
--- 默认英文输入法标识（macOS ABC）
 local default_im = 'com.apple.keylayout.ABC'
-
--- 记录上次 Insert 模式使用的输入法
 local last_im = default_im
 
--- 离开 Insert 模式 → 切回英文
-vim.api.nvim_create_autocmd('InsertLeave', {
-    callback = function()
-        -- 先记录当前输入法（如果是中文会被记录）
-        local current = vim.fn.system(im_select):gsub('%s+$', '')
-        if current ~= default_im then
-            last_im = current
-        end
-        -- 切到英文
-        vim.fn.system(im_select .. ' ' .. default_im)
-    end,
-})
+local function im_select_set(im)
+    vim.fn.jobstart({ im_select, im }, { detach = true })
+end
 
--- 进入 Insert 模式 → 恢复之前的输入法
-vim.api.nvim_create_autocmd('InsertEnter', {
-    callback = function()
-        -- 如果上次记录的是非英文输入法，恢复它
-        if last_im and last_im ~= default_im then
-            vim.fn.system(im_select .. ' ' .. last_im)
-        end
-    end,
-})
+local function im_select_get(callback)
+    local output = {}
+    vim.fn.jobstart({ im_select }, {
+        stdout_buffered = true,
+        on_stdout = function(_, data)
+            if data then
+                vim.list_extend(output, data)
+            end
+        end,
+        on_exit = function()
+            local result = table.concat(output, ''):gsub('%s+$', '')
+            callback(result)
+        end,
+    })
+end
 
--- 启动/焦点回到 nvim 时确保英文
-vim.api.nvim_create_autocmd({ 'VimEnter', 'FocusGained' }, {
-    callback = function()
-        vim.fn.system(im_select .. ' ' .. default_im)
-    end,
-})
+utils.autocmd('IMSelect', {
+    { 'InsertLeave', callback = function()
+        im_select_get(function(current)
+            if current ~= default_im then
+                last_im = current
+            end
+            im_select_set(default_im)
+        end)
+    end },
+    { 'InsertEnter', callback = function()
+        if last_im ~= default_im then
+            im_select_set(last_im)
+        end
+    end },
+    { { 'VimEnter', 'FocusGained' }, callback = function()
+        im_select_set(default_im)
+    end },
+}, true)
